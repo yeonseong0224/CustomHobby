e5c58dbcc5228defd4eba41b54cdf5ed3e68984e
yeonseong0224 <kimyeonseong0224.4@gmail.com>
2025-10-06 02:46:42 +0900

    JWT 로그인/회원가입, Controller 구현 로그인/회원가입 postman 확인함
    -> Controller 확인 요망.
    react 연결 필요

--- backend/devhobby/src/main/java/com/customhobby/backend/controller/UserController.java
+++ backend/devhobby/src/main/java/com/customhobby/backend/controller/UserController.java
@@ -3,20 +3,18 @@ package com.customhobby.backend.controller;
 import com.customhobby.backend.domain.User;
 import com.customhobby.backend.dto.*;
 import com.customhobby.backend.service.UserService;
-import com.customhobby.backend.util.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
 
+
     private final UserService userService;
-    private final JwtTokenProvider jwtTokenProvider;
+
 
     // 회원가입
     @PostMapping("/register")
@@ -27,19 +25,12 @@ public class UserController {
 
     // 로그인
     @PostMapping("/login")
-    public Map<String, Object> login(@RequestBody LoginRequestDto request) {
+    public UserResponseDto login(@RequestBody LoginRequestDto request) {
         User user = userService.login(request);
-        String token = jwtTokenProvider.createToken(user.getUserId());
-        
-        Map<String, Object> response = new HashMap<>();
-        response.put("user", new UserResponseDto(user));
-        response.put("token", token);
-        response.put("message", "로그인 성공");
-        
-        return response;
+        return new UserResponseDto(user);
     }
 
-    // 유저 조회 (JWT 인증 필요)
+    // 유저 조회
     @GetMapping("/{userId}")
     public UserResponseDto getUser(@PathVariable String userId) {
         User user = userService.findByUserId(userId)
